36b37b9fdd7c41685962e9c92b4927934483d066
Austin Grech <grech.austin@yahoo.ca>
2023-04-12 19:28:06 -0400

    fixes to js - city selection and distance now working, distance from specific address is not

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
-
 // Helper function to get the date range for the upcoming weekend
 function getUpcomingWeekendDateRange() {
   const today = new Date();
   const nextFriday = new Date(today);
-  nextFriday.setDate(today.getDate() + (5 - today.getDay()) % 7);
+  nextFriday.setDate(today.getDate() + ((5 - today.getDay()) % 7));
 
   const nextSunday = new Date(nextFriday);
   nextSunday.setDate(nextFriday.getDate() + 2);
 
-  const startDate = nextFriday.toISOString().split('T')[0];
-  const endDate = nextSunday.toISOString().split('T')[0];
+  const startDate = nextFriday.toISOString().split("T")[0];
+  const endDate = nextSunday.toISOString().split("T")[0];
 
   return { startDate, endDate };
 }
@@ -17,81 +16,99 @@ let isLocationUpdated = false;
 
 // Fetch events from Ticketmaster API for a specific date range
 async function getEvents() {
-  const apiKey = 'xahcBviTrD5wt7duynUO6G7x2HkhwDKp';
+  const apiKeyTicketMaster = "xahcBviTrD5wt7duynUO6G7x2HkhwDKp";
   const { startDate, endDate } = getUpcomingWeekendDateRange();
-  const url = `https://app.ticketmaster.com/discovery/v2/events.json?city=Toronto&startDateTime=${startDate}T00:00:00Z&endDateTime=${endDate}T23:59:59Z&apikey=${apiKey}`;
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&startDateTime=${startDate}T00:00:00Z&endDateTime=${endDate}T23:59:59Z&apikey=${apiKeyTicketMaster}`;
   const response = await fetch(url);
   const data = await response.json();
   return data._embedded.events;
 }
 // Calculate travel time and distance between two points using Bing Maps API
 async function getTimeAndDistance(origin, destination, travelMode) {
-  const apiKey = 'AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW';
-  const mode = travelMode === 'Transit' ? 'transit' : travelMode.toLowerCase();
-  const url = `https://dev.virtualearth.net/REST/v1/Routes/${mode}?wp.0=${origin}&wp.1=${destination}&key=${apiKey}`;
+  const apiKeyBingMaps =
+    "AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW";
+  const mode = travelMode === "Transit" ? "transit" : travelMode.toLowerCase();
+  const url = `https://dev.virtualearth.net/REST/v1/Routes/${mode}?wp.0=${origin}&wp.1=${destination}&key=${apiKeyBingMaps}`;
   const response = await fetch(url);
   const data = await response.json();
+  console.log(data);
   const route = data.resourceSets[0].resources[0];
   return {
     distance: route.travelDistance,
-    time: route.travelDuration
+    time: route.travelDuration,
   };
 }
-// provides auto suggestion, work in progres just provides a list of the same suggestions currently. 
+// provides auto suggestion, work in progres just provides a list of the same suggestions currently.
 async function getAutosuggestResults(query) {
-  const apiKey = 'AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW';
-  const url = `https://dev.virtualearth.net/REST/v1/Autosuggest?query=${query}&maxResults=6&key=${apiKey}`;
+  const apiKeyBingMaps =
+    "AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW";
+  const url = `https://dev.virtualearth.net/REST/v1/Autosuggest?query=${query}&maxResults=6&key=${apiKeyBingMaps}`;
   const response = await fetch(url);
   const data = await response.json();
-  return data.resourceSets[0].resources[0].value.filter(address => address.address.locality === 'Toronto');
+  return data.resourceSets[0].resources[0].value;
 }
-
 
 // // Get the location coordinates and address using Bing Maps API
 async function getLocation(address) {
-  const apiKey = 'AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW';
-  const url = `https://dev.virtualearth.net/REST/v1/Locations?query=${address}&key=${apiKey}`;
+  const apiKeyBingMaps =
+    "AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW";
+  const url = `https://dev.virtualearth.net/REST/v1/Locations?query=${address}&key=${apiKeyBingMaps}`;
   const response = await fetch(url);
   const data = await response.json();
   return data.resourceSets[0].resources[0];
 }
 
-// Handle address input and display address suggestions again work in progress cant seem to get it to display differnt address 
-async function handleAddressInput(event) {
-  const input = event.target;
-  const inputValue = input.value.trim();
-  const suggestionsContainer = document.getElementById('address-suggestions');
-  const invalidAddressWarning = document.getElementById('invalid-address-warning');
+var city = "";
 
-  if (inputValue) {
+// Handle address input and display address suggestions --- got address to work and show events for selected city.
+async function handleAddressInput() {
+  const input = document.getElementById("user-location");
+  const inputValue = input.value.trim();
+  const suggestionsContainer = document.getElementById("address-suggestions");
+  const invalidAddressWarning = document.getElementById(
+    "invalid-address-warning"
+  );
+
+  if (input && inputValue) {
     const suggestions = await getAutosuggestResults(inputValue);
-    suggestionsContainer.innerHTML = '';
+    suggestionsContainer.innerHTML = "";
 
     if (suggestions.length === 0) {
-      invalidAddressWarning.style.display = 'block';
+      invalidAddressWarning.style.display = "block";
     } else {
-      invalidAddressWarning.style.display = 'none';
+      invalidAddressWarning.style.display = "none";
 
-      suggestions.forEach(suggestion => {
-        const suggestionElement = document.createElement('div');
+      suggestions.forEach((suggestion) => {
+        const suggestionElement = document.createElement("div");
         suggestionElement.textContent = suggestion.address.formattedAddress;
-        suggestionElement.classList.add('address-suggestion');
-        suggestionElement.addEventListener('click', () => {
+        suggestionElement.classList.add("address-suggestion");
+        suggestionElement.addEventListener("click", () => {
           input.value = suggestion.address.formattedAddress;
-          suggestionsContainer.innerHTML = '';
+          suggestionsContainer.innerHTML = "";
+          city = suggestion.address.locality;
+          isLocationUpdated = true;
+          displayEvents(city);
         });
         suggestionsContainer.appendChild(suggestionElement);
       });
     }
   } else {
-    suggestionsContainer.innerHTML = '';
-    invalidAddressWarning.style.display = 'none';
+    suggestionsContainer.innerHTML = "";
+    invalidAddressWarning.style.display = "none";
   }
+  // Add event listener for input change
+  input.addEventListener("input", function () {
+    const updatedCity = input.value.trim();
+    if (isLocationUpdated && updatedCity !== city) {
+      city = updatedCity;
+      displayEvents(city);
+    }
+  });
 }
 
 // Calculate and display travel information for different travel mode
 async function calculateTravelInfo(origin, destination) {
-  const travelModes = ['Walking', 'Driving', 'Transit'];
+  const travelModes = ["Walking", "Driving", "Transit"];
   const travelInfo = {};
 
   for (const mode of travelModes) {
@@ -103,44 +120,51 @@ async function calculateTravelInfo(origin, destination) {
 }
 
 function displayTravelInfo(travelInfo) {
-  const travelInfoContainer = document.getElementById('travel-info');
-  travelInfoContainer.innerHTML = '';
+  const travelInfoContainer = document.getElementById("travel-info");
+  travelInfoContainer.innerHTML = "";
 
   for (const mode in travelInfo) {
-    const modeInfo = document.createElement('div');
-    modeInfo.textContent = `${mode}: ${travelInfo[mode].distance.toFixed(2)} km, ${travelInfo[mode].time.toFixed(0)} minutes`;
+    const modeInfo = document.createElement("div");
+    modeInfo.textContent = `${mode}: ${travelInfo[mode].distance.toFixed(
+      2
+    )} km, ${travelInfo[mode].time.toFixed(0)} minutes`;
     travelInfoContainer.appendChild(modeInfo);
   }
 }
 // Display event data and attach click event listeners to show travel information
-async function displayEvents(userLocation = 'Toronto, ON') {
-  const events = await getEvents();
-  const eventContainer = document.getElementById('event-container');
-  eventContainer.innerHTML = '';
+async function displayEvents(userLocation) {
+  const events = await getEvents(userLocation);
+  const eventContainer = document.getElementById("event-container");
+  eventContainer.innerHTML = "";
 
-  events.forEach(event => {
-    const eventElement = document.createElement('div');
-    eventElement.classList.add('event');
+  events.forEach((event) => {
+    const eventElement = document.createElement("div");
+    eventElement.classList.add("event");
 
-    const eventTitle = document.createElement('div');
-    eventTitle.classList.add('event-title');
+    const eventTitle = document.createElement("div");
+    eventTitle.classList.add("event-title");
     eventTitle.textContent = event.name;
 
-    const eventTime = document.createElement('div');
-    eventTime.classList.add('event-time');
-    eventTime.textContent = new Date(event.dates.start.dateTime).toLocaleString();
+    const eventTime = document.createElement("div");
+    eventTime.classList.add("event-time");
+    eventTime.textContent = new Date(
+      event.dates.start.dateTime
+    ).toLocaleString();
 
     eventElement.appendChild(eventTitle);
     eventElement.appendChild(eventTime);
 
-    eventElement.addEventListener('click', async () => {
-      const selectedEventContainer = document.getElementById('selected-event');
-      selectedEventContainer.innerHTML = '';
+    eventElement.addEventListener("click", async () => {
+      const selectedEventContainer = document.getElementById("selected-event");
+      selectedEventContainer.innerHTML = "";
 
       const clonedEventElement = eventElement.cloneNode(true);
       selectedEventContainer.appendChild(clonedEventElement);
 
-      const destination = event._embedded.venues[0].address.line1 + ', ' + event._embedded.venues[0].city.name;
+      const destination =
+        event._embedded.venues[0].address.line1 +
+        ", " +
+        event._embedded.venues[0].city.name;
       const travelInfo = await calculateTravelInfo(userLocation, destination);
       displayTravelInfo(travelInfo);
     });
@@ -152,12 +176,12 @@ async function displayEvents(userLocation = 'Toronto, ON') {
 // Initialize the app
 (async function init() {
   await displayEvents();
-  const saveBtn = document.getElementById('save-btn');
-  saveBtn.addEventListener('click', saveEvents);
+  const saveBtn = document.getElementById("save-btn");
+  saveBtn.addEventListener("click", saveEvents);
 
-  const updateLocationBtn = document.getElementById('update-location');
-  updateLocationBtn.addEventListener('click', async () => {
-    const userLocationInput = document.getElementById('user-location');
+  const updateLocationBtn = document.getElementById("update-location");
+  updateLocationBtn.addEventListener("click", async () => {
+    const userLocationInput = document.getElementById("user-location");
     const userLocation = userLocationInput.value.trim();
     if (userLocation) {
       const location = await getLocation(userLocation);
@@ -165,14 +189,13 @@ async function displayEvents(userLocation = 'Toronto, ON') {
         const coordinates = location.point.coordinates;
         const formattedAddress = location.address.formattedAddress;
         await displayEvents(`${coordinates[0]},${coordinates[1]}`);
-        const savedLocationDiv = document.getElementById('saved-location');
+        const savedLocationDiv = document.getElementById("saved-location");
         savedLocationDiv.textContent = `Saved location: ${formattedAddress}`;
-
       } else {
-        alert('Please enter a valid location.');
+        alert("Please enter a valid location.");
       }
     } else {
-      alert('Please enter a valid location.');
+      alert("Please enter a valid location.");
     }
   });
 })();

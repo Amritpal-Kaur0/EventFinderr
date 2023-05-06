5a8310bdfe3b069d93e992e88a68f71ae77429a2
Amrit <128442182+Amritpal-Kaur0@users.noreply.github.com>
2023-05-06 21:23:37 +0530

    descriptive comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Helper function to get the date range for the upcoming weekend
+//  function to get the date range for the upcoming weekend
 function getUpcomingWeekendDateRange() {
   const today = new Date();
   const nextFriday = new Date(today);
@@ -24,7 +24,7 @@ async function getEvents() {
   const data = await response.json();
   return data._embedded.events;
 }
-// Calculate travel time and distance between two points using Bing Maps API
+// Calculate travel time and distance between two points by Bing Maps API
 async function getTimeAndDistance(origin, destination, travelMode) {
   const apiKeyBingMaps =
     "AiIMmp6rNENDUYfZjjPyk6DynJOPYEc1cxG6szcKMSUJH889pMIC_XeVnLVyYnSW";
@@ -111,7 +111,7 @@ async function handleAddressInput() {
   locationInput.addEventListener("input", handleAddressInput);
 }
 
-// Calculate and display travel information for different travel mode
+// Calculate & display travel information for different travel mode
 async function calculateTravelInfo(origin, destination) {
   const travelModes = ["Walking", "Driving", "Transit"];
   const travelInfo = {};
@@ -140,7 +140,6 @@ function displayTravelInfo(travelInfo) {
 }
 
 // Display event data and attach click event listeners to show travel information
-// Display event data and attach click event listeners to show travel information
 async function displayEvents(userLocation) {
   // Get events based on the user location
   const events = await getEvents(userLocation);
@@ -153,23 +152,23 @@ async function displayEvents(userLocation) {
     const eventElement = document.createElement("div");
     eventElement.classList.add("event");
 
-    // Create an event title element and set its text content to the event name
+    //  TO create an event title element and set its text content to the event name
     const eventTitle = document.createElement("div");
     eventTitle.classList.add("event-title");
     eventTitle.textContent = event.name;
 
-    // Create an event time element and set its text content to the event start time
+    // to create event time element and set its text content to the event start time
     const eventTime = document.createElement("div");
     eventTime.classList.add("event-time");
     eventTime.textContent = new Date(
       event.dates.start.dateTime
     ).toLocaleString();
 
-    // Add the event title and time elements to the event element
+    // to add event title and time elements to the event element
     eventElement.appendChild(eventTitle);
     eventElement.appendChild(eventTime);
 
-    // Attach a click event listener to the event element that shows travel information
+    // to attach click event listener to the event element that shows travel information
     eventElement.addEventListener("click", async function () {
       const selectedEventContainer = document.getElementById("selected-event");
       selectedEventContainer.innerHTML = "";
@@ -185,7 +184,7 @@ async function displayEvents(userLocation) {
       displayTravelInfo(travelInfo);
     });
 
-    // Add the event element to the event container
+    // To add event element to the event container
     eventContainer.appendChild(eventElement);
   });
 }
@@ -199,27 +198,27 @@ async function filterEvents() {
   // Retrieve events for the user's location
   const events = await getEvents(userLocation);
 
-  // Clear the event container
+  // To Clear the event container
   const eventContainer = document.getElementById("event-container");
   eventContainer.innerHTML = "";
 
-  // Check which interests the user has selected
+  //  TO Check which interests the user has selected
   const sportsSelected = document.getElementById("interest-sports").checked;
   const musicSelected = document.getElementById("interest-music").checked;
   const artsTheaterSelected = document.getElementById("interest-arts").checked;
   const familySelected = document.getElementById("interest-family").checked;
 
-  // Get the user's date input
+  //  TO Get the user's date input
   const dateInput = document.getElementById("startDate").value;
   const inputDate = new Date(dateInput);
 
-  // Iterate through each event
+  //  TO Iterate through each event
   events.forEach((event) => {
     // Determine the category and family status of the event
     const category = event.classifications[0].segment.name.toLowerCase();
     const isFamily = event.classifications[0].family;
 
-    // Determine if the event should be shown based on the user's interests
+    //  TO Determine if the event should be shown based on the user's interests
     const showSports = sportsSelected && category === "sports" && !isFamily;
     const showMusic = musicSelected && category === "music" && !isFamily;
     const showArtsTheater =
@@ -228,7 +227,7 @@ async function filterEvents() {
       !isFamily;
     const showFamily = familySelected && isFamily;
 
-    // Determine if the event should be shown based on the user's date input
+    //  TO Determine if the event should be shown based on the user's date input
     const eventDate = new Date(event.dates.start.localDate);
     const showEventByDate =
       !dateInput ||
@@ -236,37 +235,37 @@ async function filterEvents() {
         eventDate.getMonth() === inputDate.getMonth() &&
         eventDate.getDate() === inputDate.getDate());
 
-    // Add the event to the event container if it should be shown
+    //  TO Add the event to the event container if it should be shown
     if (
       showEventByDate &&
       (showSports || showMusic || showArtsTheater || showFamily)
     ) {
-      // Create a new div element with the class "event"
+      //  To Create a new div element with the class "event"
       const eventElement = document.createElement("div");
       eventElement.classList.add("event");
-      // Create a new div element with the class "event-title" and set its text content to the name of the event
+      //  To Create a new div element with the class "event-title" and set its text content to the name of the event
       const eventTitle = document.createElement("div");
       eventTitle.classList.add("event-title");
       eventTitle.textContent = event.name;
-      // Create a new div element with the class "event-time" and set its text content to the start date/time of the event
+      //  TO Create a new div element with the class "event-time" and set its text content to the start date/time of the event
       const eventTime = document.createElement("div");
       eventTime.classList.add("event-time");
       eventTime.textContent = new Date(
         event.dates.start.dateTime
       ).toLocaleString();
-      // Append the event title and event time elements to the event element
+      //  TO Append the event title and event time elements to the event element
       eventElement.appendChild(eventTitle);
       eventElement.appendChild(eventTime);
-      // Add a click event listener to the event element
+      //  TO Add a click event listener to the event element
       eventElement.addEventListener("click", async function () {
-        // Get the element with the ID "selected-event" and clear its contents
+        //  TO Get the element with the ID "selected-event" and clear its contents
         const selectedEventContainer =
           document.getElementById("selected-event");
         selectedEventContainer.innerHTML = "";
         // Clone the event element and append it to the selected event container
         const clonedEventElement = eventElement.cloneNode(true);
         selectedEventContainer.appendChild(clonedEventElement);
-        // Calculate the travel information from the user's location to the event's venue and display it
+        //  TO Calculate the travel information from the user's location to the event's venue and display it
         const destination =
           event._embedded.venues[0].address.line1 +
           ", " +
@@ -279,7 +278,7 @@ async function filterEvents() {
     }
   });
 }
-// Call filterEvents to initially show all events
+// TO  Call filterEvents to initially show all events
 filterEvents();
 document.querySelectorAll('input[name="interest"]').forEach((checkbox) => {
   checkbox.addEventListener("change", function () {
@@ -287,7 +286,7 @@ document.querySelectorAll('input[name="interest"]').forEach((checkbox) => {
 
     filterEvents();
 
-    // Check if all checkboxes are unchecked and display all events if they are
+    // TO  Check if all checkboxes are unchecked and display all events if they are
     const allUnchecked = !Array.from(
       document.querySelectorAll('input[name="interest"]')
     ).some((checkbox) => checkbox.checked);
